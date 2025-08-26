b78b49ad635d8914fb26f2c766a227448a6b1907
anomie7 <anomie7777@gmail.com>
2025-08-26 23:28:39 +0900

    gh-pages 패키지 추가 및 배포 스크립트 설정: package.json에 gh-pages 추가, 배포 스크립트 추가, Vite 설정에서 출력 디렉토리를 'dist'로 변경, CNAME 파일 생성
    Signed-off-by: anomie7 <anomie7777@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
@@ -53,7 +54,7 @@
     },
     build: {
       target: 'esnext',
-      outDir: 'build',
+      outDir: 'dist',
     },
     server: {
       port: 3000,
